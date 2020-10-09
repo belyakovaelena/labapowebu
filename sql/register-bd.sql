bdff58cde3c5c8caf0ef665a111e0997bcb3da1c
belyakovaelena <72473368+belyakovaelena@users.noreply.github.com>
2020-10-09 18:26:12 +0400

    добавляю sql-damd

---
+++ sql/register-bd.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 09 2020 г., 17:19
+-- Версия сервера: 5.7.29
+-- Версия PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `register-bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`, `name`) VALUES
+(1, 'lenochka2117', 'a0ca50613a75f62a065e3c166d06312b', 'Елена'),
+(2, 'nastya1205', 'eef4e5792692ac67c1b190a8111211b4', 'Анастасия'),
+(3, 'lolka34', '256da60eed793c0414aa0489cfb20223', 'Алена'),
+(4, 'volsu20', 'afa64c8a0181d78dee764aa78b637147', 'Степан'),
+(5, 'papapa67', 'f2b17603265786b38eb715c918cb0ab3', 'Игорь'),
+(6, 'erikpick45', 'f410a366c56c501f1cba9c1ab1d67359', 'Аня');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
