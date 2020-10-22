59b263a060461e7e2a5bf130c4cbf9302ab13c35
belyakovaelena <72473368+belyakovaelena@users.noreply.github.com>
2020-10-22 22:47:51 +0400

    сделала изменения в файле с поиском

--- search.php
+++ search.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-
 function clearQuery($query)
 {
   $query = strtolower($query);
@@ -9,31 +8,29 @@ function clearQuery($query)
   return $query;
 }
 
-function searchOnPage($link, $query, $page_name)
+function searchOnPage($link, $query, $page_name)//ссылка,выражение(что ищем), ключ
 {
-  $text = file_get_contents($link);
-  $text = preg_replace('/(<(style)\b[^>]*>).*?(<\/\2>)/is', "$1$3", $text);
-  preg_match("/<body.*\/body>/s", $text, $html_body);
+  $text = file_get_contents($link);//скачивает информацию со страницы в переменную $text
 
-  $text = strip_tags($html_body[0]); // pure text from page
+  //preg_replace ищет теги и убирает
+  preg_match("/<body.*\/body>/s", $text, $html_body);//ищет в строке совпадения
+
+  $text = strip_tags($html_body[0]); // оставляет только текст, убирает теги
   $text = mb_strtolower(preg_replace('/\s+/', ' ', $text));
-
-  $pos = strpos($text, $query);
+  $pos = strpos($text, $query);//находит позиции 1 появления в строке, $text-чистый текст
 
   if ($pos === false) {
-    return false; // query doesn't exist
+    return false;
   }
-
   $searched_query = "";
 
-  if ($pos > 16)
-    $searched_query = substr($text, ($pos - 16));
+  if ($pos > 36)
+    $searched_query = substr($text, ($pos - 36));
   else
     $searched_query = substr($text, $pos);
 
-  // $searched_query = implode(" ", $searched_query);
-  $searched_query = mb_strimwidth($searched_query, 1, 120, "...", mb_internal_encoding());
-  $searched_query = str_replace($query, "<b>$query</b>", $searched_query);
+  $searched_query = mb_strimwidth($searched_query, 1, 80, "...", mb_internal_encoding());
+  $searched_query = str_replace($query, "<b>$query</b>", $searched_query);//$query-что ищем
 
   $result['content'] = $searched_query;
   $result['link'] = $link;
@@ -42,17 +39,20 @@ function searchOnPage($link, $query, $page_name)
 }
 
 $search_query = clearQuery($_POST['search']);
-$indexed_pages = [
-  "Главная страница" => "http://labapowebu/index.php#",
-  "Меню" => "http://labapowebu/menu.php",
-  "доставка" => "http://labapowebu/delivery.php",
-  "Кабинет" => "http://labapowebu/register.php",
 
+$indexed_pages = [ //ключи
+    "menu" => "http://labapowebu//html/menu.html",
+    "delivery" => "http://labapowebu//html/delivery.html",
+    "Главная страница" => "http://labapowebu/index.php#",
+    "Меню" => "http://labapowebu/menu.php",
+    "доставка" => "http://labapowebu/delivery.php",
+    "Кабинет" => "http://labapowebu/register.php",
 ];
+$indexed_pages = array_slice(scandir('./html/'), 2);
 $results = [];
 
-foreach (array_keys($indexed_pages) as $item) {
-  $result = searchOnPage($indexed_pages[$item], $search_query, $item);
+foreach ($indexed_pages as $item) {
+  $result = searchOnPage("./html/" . $item, $search_query, ' ');
   if ($result !== false) {
     $results[] = $result;
   }
@@ -60,7 +60,6 @@ foreach (array_keys($indexed_pages) as $item) {
 
 $_SESSION['search_results'] = $results;
 $_SESSION['debug'] = $_SERVER['HTTP_REFERER'];
-
 $return_link = $_SERVER['HTTP_REFERER'];
 
 header("Location: answer.php");
