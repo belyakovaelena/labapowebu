981409194d5d7d84d61a1437fc3949b80d770a9f
belyakovaelena <72473368+belyakovaelena@users.noreply.github.com>
2020-12-02 15:18:49 +0400

    сделала sql-иньекции

--- add_dish.php
+++ add_dish.php
@@ -1,18 +1,19 @@
 <?php
-session_start();
+require_once "connect.php";
 if(!isset($_SESSION['user']))
 header('Location: ../register.php');
 else{
-    $dish = filter_var(trim($_POST['dish']), FILTER_SANITIZE_STRING);
-    $price = filter_var(trim($_POST['price']), FILTER_SANITIZE_STRING);
-    $weight = filter_var(trim($_POST['weight']), FILTER_SANITIZE_STRING);
-    $calories = filter_var(trim($_POST['calories']), FILTER_SANITIZE_STRING);
-    $time = filter_var(trim($_POST['time']), FILTER_SANITIZE_STRING);
+
+    $dish = mysqli_real_escape_string($mysql, trim($_POST['dish']));
+    $price = mysqli_real_escape_string($mysql, trim($_POST['price']));
+    $weight = mysqli_real_escape_string($mysql, trim($_POST['weight']));
+    $calories = mysqli_real_escape_string($mysql, trim($_POST['calories']));
+    $time = mysqli_real_escape_string($mysql, trim($_POST['time']));
     $user = $_SESSION['login'];
 
 
 
-    require_once "connect.php";
+
     $mysql->query("INSERT INTO `dish` (`dish`, `price`, `weight`, `calories`, `time`,`user`)VALUES('$dish', '$price', '$weight', '$calories', '$time', '$user')");
 
 
--- authorization.php
+++ authorization.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
+require_once "connect.php";
 $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 $pass = md5($pass."ktyf0301");
-$mysql = new mysqli('localhost', 'root', 'root', 'register-bd');
+
 
 $result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass`='$pass'");
 
